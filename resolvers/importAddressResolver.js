8adad479ebd55e89e62851cb488c46e62950b144
tarix <tarix2006@gmail.com>
2020-07-31 09:52:31 +0200

    - improve for search in import  erc20

--- resolvers/importAddressResolver.js
+++ resolvers/importAddressResolver.js
@@ -40,61 +40,83 @@ module.exports = {
      * */
     updateLabelFromEther: async (parent, { type }, { db }, info) => {
       try {
-        let link = type === 7 ? "https://etherscan.io/tokens?q=a&ps=100&p=" : null;
-        link = type === 8 ? "https://etherscan.io/tokens-nft?q=+&ps=100&p=" : link;
+        // let link = type === 7 ? "https://etherscan.io/tokens?q=a&ps=100&p=" : null;
+        // link = type === 8 ? "https://etherscan.io/tokens-nft?q=+&ps=100&p=" : link;
         // link =
         //   type === 7
         //     ? "https://etherscan.io/accounts/label/exchange?subcatid=undefined&size=100&start=1&col=1&order=asc"
         //     : link;
-        let i = 1;
+        let char = type === 7 ? 1 : 37;
         let importAddresses = [];
+        const brakeResult = [];
         do {
-          importAddresses = [];
-          const results = await axios.get(`${link}${i}`, {
-            // params: {
-            //   q: "+",
-            //   ps: 100,
-            //   p: i
-            // },
-            responseType: "document",
-            headers: {
-              "content-type":
-                "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
-              cookie: "__cfduid=d9576c8dab355bb823ca66298c7ecc1161596125839; ASP.NET_SessionId=f1lt233x25iifhonohdmwdgz"
+          const letter = (char + 10).toString(36);
+          let i = 1;
+          do {
+            importAddresses = [];
+            let link = "";
+            if (type === 7) {
+              link = `https://etherscan.io/tokens?q=${letter}&ps=100&p=${i}`;
             }
-          });
-
-          if (results.data) {
-            // prepare data
-            const root = HTMLParser.parse(results.data);
-            const table = root.querySelectorAll("tbody tr");
-            importAddresses = map(table, row => {
-              const hash = row.querySelector("a").text;
-              const name = row.querySelectorAll("td")[1].text;
-              const symbol = row.querySelectorAll("td")[2].text;
-              return {
-                hash,
-                name,
-                labelId: type,
-                symbol,
-                category: type === 7 ? "ERC20" : "ERC721"
-              };
+            if (type === 8) {
+              link = `https://etherscan.io/tokens-nft?q=+&ps=100&p=${i}`;
+            }
+            if (type === 6) {
+              // needed credentials
+              link = `https://etherscan.io/accounts/label/exchange?subcatid=undefined&size=100&start=${(i - 1) * 100 +
+                1}&col=1&order=asc`;
+            }
+            const results = await axios.get(link, {
+              // params: {
+              //   q: "+",
+              //   ps: 100,
+              //   p: i
+              // },
+              responseType: "document",
+              headers: {
+                "content-type":
+                  "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+                cookie:
+                  "__cfduid=d9576c8dab355bb823ca66298c7ecc1161596125839; ASP.NET_SessionId=f1lt233x25iifhonohdmwdgz"
+              }
             });
 
-            if (importAddresses.length) {
-              // await db.import_address_label.bulkCreate(importAddresses);
-              for (let j = 0; j < importAddresses.length; j++) {
-                await db.import_address_label.findOrCreate({
-                  where: { hash: importAddresses[j].hash },
-                  defaults: importAddresses[j]
-                });
+            if (results.data) {
+              // prepare data
+              const root = HTMLParser.parse(results.data);
+              const table = root.querySelectorAll("tbody tr");
+              importAddresses = map(table, row => {
+                const hash = row.querySelector("a").text;
+                const name = row.querySelectorAll("td")[1].text;
+                const symbol = row.querySelectorAll("td")[2].text;
+                return {
+                  hash,
+                  name,
+                  labelId: type,
+                  symbol,
+                  category: type === 7 ? "ERC20" : "ERC721"
+                };
+              });
+
+              if (importAddresses.length) {
+                // await db.import_address_label.bulkCreate(importAddresses);
+                for (let j = 0; j < importAddresses.length; j++) {
+                  await db.import_address_label.findOrCreate({
+                    where: { hash: importAddresses[j].hash },
+                    defaults: importAddresses[j]
+                  });
+                }
               }
+              await sleep(1000 + 1000 * Math.trunc(Math.random() * 10));
             }
-            await sleep(1000 + 1000 * Math.trunc(Math.random() * 10));
-          }
-          i = i + 1;
-          console.log(i);
-        } while (importAddresses.length);
+            i = i + 1;
+          } while (importAddresses.length);
+
+          brakeResult.push(i);
+          console.log(brakeResult);
+          char = char + 1;
+        } while (char < 37);
+
         return {
           success: true,
           message: `Imported 0`
